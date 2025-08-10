d9b21c3b32e46edf00fd569ffb370d9326c0c0b4
mykolamuti <satoshikonpaprika@gmail.com>
2025-08-10 11:12:24 +0330

    Add files via upload

---
+++ src/data/features.json.ts
@@ -0,0 +1,12 @@
+export interface Template {
+  url: string;
+  title: string;
+};
+export const templates: Template[] = [
+  { url: "https://superbx.app", title: "SUPERB" },
+  { url: "#", title: "FluxKit" },
+  { url: "#", title: "Pinstripe UI" },
+  { url: "#", title: "Rover CLI" },
+  { url: "#", title: "Timestack" },
+  { url: "#", title: "Signalboard" }
+];
